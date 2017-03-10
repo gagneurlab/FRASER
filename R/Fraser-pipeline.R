1d47726d0b188a3bfb0a658e5f270ad13b69b8b6
Christian Mertes <mertes@in.tum.de>
2017-03-10 19:08:08 +0100

    removing trailing whitespaces and add docs for TUM/Stanford Travel

--- R/Fraser-pipeline.R
+++ R/Fraser-pipeline.R
@@ -1,41 +1,41 @@
 ##
 ## @author Christian Mertes \email{mertes@@in.tum.de}
-## 
+##
 ## This file contains the standard pipeline for a FraseR analysis
 ##
-
-#' 
-#' @title Find rare splicing events within RNA-seq data
+#'
+#' Find rare splicing events within RNA-seq data
+#'
 #' @description This function performs a default analysis of RNA-seq data
-#'             
-#' @param settings A FraseRSetting object with all the information 
+#'
+#' @param settings A FraseRSetting object with all the information
 #'             how and what to count
-#' @param internBPPARAM A BiocParallel param object to configure the 
+#' @param internBPPARAM A BiocParallel param object to configure the
 #'             parallel backend of the internal loop for counting
-#'             
-#' @return FraseRDataSet 
+#'
+#' @return FraseRDataSet
 #' @export
 #' @examples
 #'   fds <- FraseR(createTestFraseRSettings())
 #'   plotSampleResults(fds)
 FraseR <- function(settings, internBPPARAM=SerialParam()){
-    
+
     # Check input
     stopifnot(class(settings) == "FraseRSettings")
     stopifnot(is(internBPPARAM, "BiocParallelParam"))
-    
+
     # count data
     fds <- countRNAData(settings, internBPPARAM=internBPPARAM)
-    
+
     # calculate PSI values
     fds <- calculatePSIValues(fds)
-    
+
     # calculate ZScores
     fds <- calculateZScores(fds)
-    
+
     # calculte P-values
     fds <- calculatePValues(fds)
-    
+
     # return final analysis
     return(fds)
 }
