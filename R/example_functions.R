d26df2bddce5d3ad40b4199a43d8cbc31f30e7da
Christian Mertes <mertes@in.tum.de>
2017-03-13 13:40:44 +0100

    add load and save function for HDF5Arrays

--- R/example_functions.R
+++ R/example_functions.R
@@ -1,30 +1,30 @@
 #'
 #' Create a test case dataset (sample information only)
-#' to be used in the vignette and to explore the 
+#' to be used in the vignette and to explore the
 #' functionallity of the FraseR package.
-#' 
+#'
 #' @return a FraseRSettings object which contains a test case
 #' @export
-#' @examples 
+#' @examples
 #'     createTestFraseRSettings()
-#'     
+#'
 createTestFraseRSettings <- function(){
     # get sample data table
     sampleTable <- fread(system.file(
                 "extdata", "sampleTable.tsv", package="FraseR", mustWork=TRUE
     ))
-    
+
     # convert it to a bamFile list
-    sampleTable[,bamFile:=unlist(BamFileList(system.file(
-                sampleTable[,bamFile], package="FraseR", mustWork=TRUE
-    )))]
-    
-    # check that NHDF is NA group 
+    bamFiles <- system.file(sampleTable[,bamFile],
+            package="FraseR", mustWork=TRUE)
+    sampleTable[,bamFile:=unlist(BamFileList(bamFiles))]
+
+    # check that NHDF is NA group
     sampleTable[gene=='NHDF',group:=NA]
-    
+
     # return a FraseRSettings object
     return(FraseRSettings(
-        sampleData=sampleTable, 
+        sampleData=sampleTable,
         outputFolder=file.path(Sys.getenv("HOME"), "FraseR")
     ))
 }
@@ -33,29 +33,29 @@ createTestFraseRSettings <- function(){
 #' Create a test case dataset based on the test sample annotation
 #' filled with counts to be used in the vignette and
 #' to explore the functionallity of the FraseR package.
-#' 
+#'
 #' @return a FraseRDataSet object which contains a test case
 #' @export
-#' @examples 
+#' @examples
 #'     createTestFraseRDataSet()
-#'     
+#'
 createTestFraseRDataSet <- function(BPPARAM=NULL){
-    
+
     # get test sample annotation
     settings <- createTestFraseRSettings()
-    
+
     # set paralelle settings if given
     if(!is.null(BPPARAM)){
         settings@parallel <- BPPARAM
     }
-    
+
     # count data
     dataset <- countRNAData(settings)
-    
+
     # calculate PSI values
     dataset <- calculatePSIValues(dataset)
     dataset <- calculateSitePSIValue(dataset)
-    
+
     # return a FraseRDataSet object
     return(dataset)
 }
