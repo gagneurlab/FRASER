943edafc841bd105e77a84ec49b1dab2cc83c529
Christian Mertes <mertes@in.tum.de>
2017-03-20 19:13:20 +0100

    add hdf5 to betabinomial testing add parallel for visualization

--- R/plotResults.R
+++ R/plotResults.R
@@ -4,25 +4,26 @@
 #' based on plotly.
 #'
 #' @export
-#' @examples 
+#' @examples
 #'      plotSampleResults(dataset, "sample1")
 #'      plotSampleResults(dataset, "sample1", "result.html")
-plotSampleResults <- function(dataset, sampleID=NULL, 
+plotSampleResults <- function(dataset, sampleID=NULL,
                     file=NULL, browseIt=FALSE){
-    
+    require(FraseR)
+
     #
     # check input
     stopifnot(class(dataset) == "FraseRDataSet")
-    
+
     # if sample is empty create all plots for all samples
     if(is.null(sampleID)){
         samples2plot <- !is.na(sampleGroup(dataset@settings))
         sampleIDs2plot <- samples(dataset@settings[samples2plot])
-        return(sapply(sampleIDs2plot, plotSampleResults, 
-                dataset=dataset, file=file
+        return(bplapply(sampleIDs2plot, plotSampleResults,
+                dataset=dataset, file=file, BPPARAM=parallel(dataset@settings)
         ))
     }
-    
+
     # check the rest
     stopifnot(sampleID %in% samples(dataset@settings))
     if(is.null(file)){
@@ -31,17 +32,17 @@ plotSampleResults <- function(dataset, sampleID=NULL,
             file <- file.path(outDir, paste0(sampleID, "-FraseR-results.html"))
         }
     }
-     
+
     # create folder if needed
     if(!is.null(file) && !dir.exists(dirname(file))){
         dir.create(dirname(file), recursive=TRUE)
     }
-    
+
     # generate each sub plot
     psi3plot <- .plotVolcano(dataset, sampleID, "splitReads", "psi3", 1)
     psi5plot <- .plotVolcano(dataset, sampleID, "splitReads", "psi5", 2)
     psisplot <- .plotVolcano(dataset, sampleID, "nonSplicedReads", "sitePSI", 3)
-    
+
     # combine plots
     mainplot <- plotly::subplot(psi3plot, psi5plot, psisplot,
             nrows = 3, shareX = FALSE, shareY = TRUE,
@@ -49,7 +50,7 @@ plotSampleResults <- function(dataset, sampleID=NULL,
     ) %>% layout(showlegend = FALSE,
             title = paste0("FraseR results for sample: <b>", sampleID, "</b>")
     )
-    
+
     #
     if(!is.null(file)){
         saveWidget(mainplot, file=file)
@@ -58,25 +59,35 @@ plotSampleResults <- function(dataset, sampleID=NULL,
         }
         return(file)
     }
-    
+
     # return it
     return(mainplot)
-    
 }
 
 #'
 #' generate a volcano plot
 #'
 #' @noRd
-.plotVolcano <- function(dataset, sampleID, readType, psiType, subID, 
+.plotVolcano <- function(dataset, sampleID, readType, psiType, subID,
                     ylim=c(0,30), xlim=c(-5,5)){
     curSlot <- slot(dataset, readType)
     zscore  <- assays(curSlot)[[paste0("zscore_", psiType)]][,sampleID]
     pvalue  <- -log10(assays(curSlot)[[paste0("pvalue_", psiType)]][,sampleID])
-    
+    psival  <- assays(curSlot)[[psiType]][,sampleID]
+
+    if("DelayedMatrix" %in% is(zscore)){
+        zscore <- as.vector(zscore[,1])
+    }
+    if("DelayedMatrix" %in% is(pvalue)){
+        pvalue <- as.vector(pvalue[,1])
+    }
+    if("DelayedMatrix" %in% is(psival)){
+        psival <- as.vector(psival[,1])
+    }
+
     # remove NAs from data
     toplot <- !is.na(zscore) & !is.na(pvalue) & !is.infinite(pvalue)
-    
+
     # remove unsignificant data (to keep plotly responsive)
     # max 50k points
     xCutoff <- 1.5
@@ -86,52 +97,69 @@ plotSampleResults <- function(dataset, sampleID=NULL,
         unsigni <- unsigni & rank(pvalue[toplot]) > 50000
     }
     toplot[toplot]  <- !unsigni
-    
-    
-    # trim data to ylim and xlim 
+
+
+    # trim data to ylim and xlim
     pvalue2plot <- pvalue
     zscore2plot <- zscore
-    pvalue2plot[toplot & pvalue2plot > max(ylim)] <- max(ylim) - 1
-    zscore2plot[toplot & zscore2plot > max(xlim)] <- max(xlim) - 1
-    zscore2plot[toplot & zscore2plot < min(xlim)] <- min(xlim) + 1
-    
-    p <- plot_ly(x=zscore2plot[toplot], y=pvalue2plot[toplot], type="scattergl",
-            
+    pvalue2plot[toplot & pvalue2plot > max(ylim)] <- max(ylim)
+    zscore2plot[toplot & zscore2plot > max(xlim)] <- max(xlim)
+    zscore2plot[toplot & zscore2plot < min(xlim)] <- min(xlim)
+    plotTraces <- list(
+        "&#968; <= 30%"=.na2false(psival[toplot]<=0.3),
+        "60% >= &#968; > 30%"=.na2false(psival[toplot]<=0.6 & psival[toplot]>0.3),
+        "&#968; > 60%"=.na2false(psival[toplot]>0.6),
+        "&#968; == NA"=is.na(psival[toplot])
+    )
+
+    p <- plot_ly()
+    for(i in seq_along(plotTraces)){
+        t <- which(toplot)[plotTraces[[i]]]
+
+        p <- p %>% add_trace(type="scattergl",
+            x=zscore2plot[t], y=pvalue2plot[t],
             mode = "markers",
-            marker = list(   
-                color = pvalue2plot[toplot],
+            marker = list(
+                color = pvalue2plot[t],
                 cmin = 0,
                 cmax = max(ylim),
                 colorbar = list(yanchor = "middle")
             ),
-            name = psiType,
+            name = paste0(psiType, ": ", names(plotTraces)[i]),
+            visible = ifelse(i<=2, TRUE, "legendonly"),
             text = paste0(
-                "Symbol:     ", mcols(curSlot)$hgnc_symbol[toplot], "</br>",
-                "Chromosome: ", seqnames(curSlot)[toplot],  "</br>",
-                "Start:      ", start(curSlot)[toplot],     "</br>",
-                "End:        ", end(curSlot)[toplot],       "</br>",
-                "-log<sub>10</sub>(<i>P</i>-value): ", round(pvalue[toplot], 2),
-                "</br>",
-                "Z-score:    ", round(zscore[toplot], 2), "</br>"
+                "Symbol:     ", mcols(curSlot)$hgnc_symbol[t], "</br>",
+                "Chromosome: ", seqnames(curSlot)[t],  "</br>",
+                "Start:      ", start(curSlot)[t],     "</br>",
+                "End:        ", end(curSlot)[t],       "</br>",
+                "-log<sub>10</sub>(<i>P</i>-value): ", round(pvalue[t], 2), "</br>",
+                "Z-score:    ", round(zscore[t], 2), "</br>",
+                "PSI-value:  ", round(psival[t], 3)*100, "%</br>"
             )
-    )
+        )
+    }
+
+    nxlim <- c(xlim[1]*1.05, xlim[2]*1.05)
+    nylim <- c(ylim[1], ylim[2]*1.05)
+
     p <- p %>% layout(
-        xaxis=list(range=xlim, title=paste0("Z-score of splicetype: ",psiType)),
-        yaxis=list(range=ylim, title="-log<sub>10</sub><i>P</i>-value"),
-        annotations=list(
-            x=0, 
-            y=yCutoff/2, 
-            text="",
+        xaxis=list(range=nxlim, title=paste0("Z-score of splicetype: ",psiType)),
+        # TODO P-value does not appear in italic
+        yaxis=list(range=nylim, title="-log<sub>10</sub> P-value")
+        #annotations=list(
+        #    x=0,
+        #    y=yCutoff/2,
+        #    text="",
             #text=paste("Removed points</br>",
-            #    round(sum(unsigni)/length(unsigni)*100, digits=2), 
+            #    round(sum(unsigni)/length(unsigni)*100, digits=2),
             #    "% of points"
             #),
-            xref = paste0("x", subID),
-            yref = paste0("y", subID),
-            showarrow = FALSE
-        )
+        #    xref = paste0("x", subID),
+        #    yref = paste0("y", subID),
+        #    showarrow = FALSE
+        #)
     )
-    
+
     # add removed area
     #p <- p %>% layout(shapes = list(
     #        list(type = "rect",
@@ -140,9 +168,11 @@ plotSampleResults <- function(dataset, sampleID=NULL,
     #                y0 = 0, y1 = yCutoff, yref = paste0("y", subID)
     #        )
     #))
-    
+
     return(p)
 }
 
-
-
+.na2false <- function(x){
+    x[is.na(x)] <- FALSE
+    return(x)
+}
