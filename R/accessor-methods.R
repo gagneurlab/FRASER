155445c810a7400d9a66ddff72f58f832b31f6eb
Christian Mertes <mertes@in.tum.de>
2017-03-22 17:27:48 +0100

    add accessor method file

---
+++ R/accessor-methods.R
@@ -0,0 +1,20 @@
+#'
+#' @author Christian Mertes
+#'
+#' This file contains accessor functions for the FraseRDataSet object
+#'
+
+.getReadTypeFromPsiType <- function(psiType){
+    stopifnot(length(psiType == 1 && class(psiType) == "")
+    readType <- switch(psiType,
+           psi3="spliceSite",
+           psi5="spliceSite",
+           sitePSI="nonSplicedReads",
+           stop("The given PSI type '", psiType, "' is not correct!")
+    )
+    return(readType)
+}
+
+fdsmcols <- function(fds, psiType){
+    assays(fds, )
+}
