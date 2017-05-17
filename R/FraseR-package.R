3fd7de0dd854bbf00a0edd3b23ae62f1cbe951b2
Christian Mertes <mertes@in.tum.de>
2017-05-17 22:23:55 +0200

    play around with depends and import

--- R/FraseR-package.R
+++ R/FraseR-package.R
@@ -10,8 +10,7 @@
 ### Parallel computing
 #'
 #' @import BiocParallel
-#' @importFrom BiocParallel SerialParam
-#' @importFrom BiocParallel MulticoreParam
+#' @importFrom BiocParallel SerialParam MulticoreParam
 #'
 #'
 ### GRange/Experiment/bamFile packages
@@ -19,30 +18,21 @@
 #' @import GenomicAlignments
 #' @import SummarizedExperiment
 #' @import GenomicRanges
-#' @importFrom IRanges subsetByOverlaps
-#' @importFrom IRanges from
-#' @importFrom IRanges to
-#' @importFrom IRanges IRanges
+#' @importFrom IRanges subsetByOverlaps from to IRanges
 #' @importFrom Rsubread featureCounts
-#' @importFrom Rsamtools ScanBamParam
-#' @importFrom Rsamtools scanBamHeader
-#' @importFrom Rsamtools bamMapqFilter
-#' @importFrom Rsamtools bamWhich
-#' @importFrom Rsamtools bamWhich<-
+#' @importFrom Rsamtools ScanBamParam scanBamHeader
+#' @importFrom Rsamtools bamMapqFilter bamWhich bamWhich<-
 #'
 #'
 ### Annotation
 #'
-#' @importFrom biomaRt useEnsembl
-#' @importFrom biomaRt getBM
+#' @importFrom biomaRt useEnsembl getBM
 #'
 #'
 ### Plotting
 #'
 #' @importFrom graphics plot
-#' @importFrom plotly plot_ly
-#' @importFrom plotly subplot
-#' @importFrom plotly layout
+#' @importFrom plotly plot_ly subplot layout
 #' @importFrom htmlwidgets saveWidget
 #' @importFrom HDF5Array writeHDF5Array
 #' @importFrom DelayedArray rowMeans
@@ -51,43 +41,24 @@
 #'
 ### Data handling
 #'
-#' @importFrom data.table data.table
-#' @importFrom data.table as.data.table
-#' @importFrom data.table :=
-#' @importFrom data.table fread
-#' @importFrom data.table setnames
-#' @importFrom data.table is.data.table
+#' @importFrom data.table data.table as.data.table is.data.table
+#' @importFrom data.table := fread setnames
 #' @import tidyr
 #' @importFrom HDF5Array writeHDF5Array
 #'
 #'
 ### P-Value calculation
 #'
-#' @importFrom stats sd
-#' @importFrom stats rbinom
-#' @importFrom stats fisher.test
-#' @importFrom stats na.omit
-#' @importFrom VGAM rbetabinom
-#' @importFrom VGAM vglm
-#' @importFrom VGAM Coef
-#' @importFrom VGAM pbetabinom
-#' @importFrom VGAM betabinomial
+#' @importFrom stats sd rbinom fisher.test na.omit
+#' @importFrom VGAM rbetabinom vglm Coef pbetabinom betabinomial
 #'
 ### Miscelenious functions
 #'
-#' @importFrom BBmisc isScalarCharacter
-#' @importFrom BBmisc isScalarLogical
+#' @importFrom BBmisc isScalarCharacter isScalarLogical
 #' @importFrom R.utils renameFile
 #' @importFrom tools file_path_as_absolute
-#' @importFrom methods as
-#' @importFrom methods callNextMethod
-#' @importFrom methods is
-#' @importFrom methods new
-#' @importFrom methods slot
-#' @importFrom methods slot<-
-#' @importFrom methods validObject
-#' @importFrom utils browseURL
-#' @importFrom utils capture.output
+#' @importFrom methods as callNextMethod is new slot slot<- validObject
+#' @importFrom utils browseURL capture.output
 #'
 #'
 
