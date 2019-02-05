b7b9a70c8485e27e9a145ec9fbdcf0363f99c6fc
Ines Scheller <scheller@in.tum.de>
2019-02-05 15:03:11 +0100

    small changes

--- R/pvalsNzscore.R
+++ R/pvalsNzscore.R
@@ -3,7 +3,7 @@
 # Function to calculate the z-scores
 calculateZscore <- function(fds){
   
-  mu <- predictMu(fds, all=TRUE)
+  mu <- predictMu(fds) 
   psi <- t(plogis(x(fds, all=TRUE)))
   log2fc <- log2(psi) - log2(mu)
   
@@ -17,14 +17,16 @@ calculateZscore <- function(fds){
 }
 
 # Function to calculate the p-values
-calculatePvalues <- function(fds){
+calculatePvalues <- function(fds, BPPARAM=bpparam()){
   
-  mu <- predictMu(fds, all=TRUE)
+  mu <- predictMu(fds) 
   rho <- matrix(rho(fds), nrow = nrow(mu) , ncol = ncol(mu))
   k <- as.matrix(K(fds))
   n <- as.matrix(N(fds))
   
-  pval <- VGAM::pbetabinom(k, n, mu, rho)
+  #pval <- VGAM::pbetabinom(k, n, mu, rho)
+  pval_list <- bplapply(seq_len(nrow(fds)), singlePvalue, k=k, n=n, mu=mu, rho=rho, BPPARAM=BPPARAM)
+  pval <- matrix(unlist(pval_list), nrow=length(pval_list), byrow = TRUE)
   dval <- VGAM::dbetabinom(k, n, mu, rho)
   pvals <- 2 * pmin(0.5, pval + dval, 1 - pval + dval)
   
@@ -33,4 +35,15 @@ calculatePvalues <- function(fds){
   pVals(fds) <- pvalMat
   
   return(fds)
+}
+
+singlePvalue <- function(idx, k, n, mu, rho){
+  
+  ki <- k[idx,]
+  ni <- n[idx,]
+  mui <- mu[idx,]
+  rhoi <- rho[idx]  
+  
+  pvals <- pbetabinom(ki, ni, mui, rhoi)
+  return (pvals)
 }
