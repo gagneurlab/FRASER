24d5c070728982e0cd5ccbd80db8c8eb20030bd0
Christian Mertes <mertes@in.tum.de>
2019-02-06 23:35:48 +0100

    adapt pvals and zscores to new types

--- R/pvalsNzscore.R
+++ R/pvalsNzscore.R
@@ -2,35 +2,35 @@
 
 # Function to calculate the z-scores
 calculateZscore <- function(fds){
-  
-  mu <- predictMu(fds, all=TRUE)
-  psi <- t(plogis(x(fds, all=TRUE)))
+
+  mu <- predictedMeans(fds)
+  psi <- t(plogis(x(fds)))
   log2fc <- log2(psi) - log2(mu)
-  
+
   # z = ( x - mean ) / sd
   zscores <- (log2fc - rowMeans(log2fc)) / rowSds(log2fc)
-  
+
   zScores(fds) <- zscores
-  
+
   return(fds)
-  
+
 }
 
 # Function to calculate the p-values
 calculatePvalues <- function(fds){
-  
-  mu <- predictMu(fds, all=TRUE)
-  rho <- matrix(rho(fds), nrow = nrow(mu) , ncol = ncol(mu))
+
+  mu <- as.matrix(predictedMeans(fds))
+  rho <- matrix(rho(fds), nrow=nrow(mu) , ncol=ncol(mu))
   k <- as.matrix(K(fds))
   n <- as.matrix(N(fds))
-  
-  pval <- VGAM::pbetabinom(k, n, mu, rho)
-  dval <- VGAM::dbetabinom(k, n, mu, rho)
-  pvals <- 2 * pmin(0.5, pval + dval, 1 - pval + dval)
-  
-  pvalMat <- matrix(pvals, nrow=dim(fds)[1], ncol=dim(fds)[2])
-  
+
+  pval <- pbetabinom(k, n, mu, rho)
+  dval <- dbetabinom(k, n, mu, rho)
+  pvals <- 2 * pmin(0.5, pval, 1 - pval + dval)
+
+  pvalMat <- matrix(pvals, nrow=dim(mu)[1], ncol=dim(mu)[2])
+
   pVals(fds) <- pvalMat
-  
+
   return(fds)
 }
