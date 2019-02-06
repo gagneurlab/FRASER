9a9b6a24c0bde5eef5202f59dd5b58e4fc4b7a3d
Christian Mertes <mertes@in.tum.de>
2019-02-07 00:00:32 +0100

    update testing script

--- inst/extdata/example_script.R
+++ inst/extdata/example_script.R
@@ -1,19 +1,41 @@
 devtools::document()
 devtools::load_all()
-devtools::install(".")
-unloadNamespace("FraseR")
-
-library("FraseR")
 
 bpparam <- SnowParam(workers=8, progressbar=TRUE)
 bpparam <- SerialParam()
 register(bpparam)
 
+# real data
 fds <- loadFraseRDataSet("~/projects/data", "kremer-bader-et-al", upgrade=TRUE)
-probE <- 0.01
-featureExclusionMask(fds) <- sample(c(TRUE, FALSE), nrow(fds), replace=TRUE, prob=c(probE,1-probE))
-table(featureExclusionMask(fds))
+probE <- 0.1
+q <- 24
+
+# toy data
+fds <- makeExampleFraseRDataSet()
+probE <- 0.5
+q <- 3
+
 debug(fitAutoencoder)
-fds <- fitAutoencoder(fds, q=24, verbose=TRUE, BPPARAM=bpparam)
+debug(subsetFraseR)
+debug(singleDFit)
+debug(updateD)
+
+type <- "psiSite"
+for(type in c("psi5", "psi3", "psiSite")){
+
+    # subset fitting
+    featureExclusionMask(fds, type=type) <- sample(c(TRUE, FALSE),
+            nrow(mcols(fds, type=type)), replace=TRUE, prob=c(probE,1-probE))
+    table(featureExclusionMask(fds, type="psiSite"))
+
+    # run autoencoder
+    fds <- fitAutoencoder(fds, q=q, type=type, verbose=TRUE, BPPARAM=bpparam, iterations=5)
+
+    # calculate stats
+    fds <- calculateZscore(fds, type=type)
+    fds <- calculatePvalues(fds, type=type)
+}
+
+
 
 
