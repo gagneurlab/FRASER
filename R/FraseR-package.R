8035060ea8eafd67364c602838d797f66e469fe0
Christian Mertes <mertes@in.tum.de>
2019-04-17 00:20:20 +0200

    add PRROC to package

--- R/FraseR-package.R
+++ R/FraseR-package.R
@@ -87,6 +87,7 @@
 #' @importFrom DelayedMatrixStats rowMedians rowSds
 #' @importFrom stats runif median quantile
 #' @importFrom extraDistr rdirmnom
+#' @importFrom PRROC pr.curve
 #'
 #' @useDynLib FraseR
 #'
