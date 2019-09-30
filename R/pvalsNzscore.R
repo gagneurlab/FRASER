381c12320cf5c51e946b9168d2fe1b1d79f22ab4
Christian Mertes <mertes@in.tum.de>
2019-09-30 21:46:18 +0200

    documentation + cleanup

--- R/pvalsNzscore.R
+++ R/pvalsNzscore.R
@@ -28,7 +28,8 @@ calculatePvalues <- function(fds, type=currentType(fds),
 
     # make sure its only in-memory data for k and n
     currentType(fds) <- type
-    counts(fds, type=type, side="other", HDF5=FALSE) <- as.matrix(N(fds) - K(fds))
+    counts(fds, type=type, side="other", HDF5=FALSE) <-
+            as.matrix(N(fds) - K(fds))
     counts(fds, type=type, side="ofInterest", HDF5=FALSE) <- as.matrix(K(fds))
 
     # if method BB is used take the old FraseR code
@@ -56,16 +57,19 @@ calculatePvalues <- function(fds, type=currentType(fds),
     pval <- do.call(rbind, pval_list)
     dval <- dbetabinom(k, n, mu, rho)
     pvals <- 2 * pmin(pval, 1 - pval + dval, 0.5)
-    fwer_pval <- bplapply(seq_len(ncol(pvals)), adjust_FWER_PValues, pvals=pvals, index, BPPARAM=BPPARAM)
+    fwer_pval <- bplapply(seq_len(ncol(pvals)), adjust_FWER_PValues,
+            pvals=pvals, index, BPPARAM=BPPARAM)
     fwer_pvals <- do.call(cbind, fwer_pval)
     pVals(fds, dist="BetaBinomial") <- fwer_pvals
 
     # binomial p-values
-    pval_list <- bplapply(seq_len(nrow(mu)), singlePvalueBinomial, k=k, n=n, mu=mu, BPPARAM=BPPARAM)
+    pval_list <- bplapply(seq_len(nrow(mu)), singlePvalueBinomial, k=k, n=n,
+            mu=mu, BPPARAM=BPPARAM)
     pval <- do.call(rbind, pval_list)
     dval <- dbinom(k, n , mu)
     pvals <- 2 * pmin(pval, 1 - pval + dval, 0.5)
-    fwer_pval <- bplapply(seq_len(ncol(pvals)), adjust_FWER_PValues, pvals=pvals, index, BPPARAM=BPPARAM)
+    fwer_pval <- bplapply(seq_len(ncol(pvals)), adjust_FWER_PValues,
+            pvals=pvals, index, BPPARAM=BPPARAM)
     fwer_pvals <- do.call(cbind, fwer_pval)
     pVals(fds, dist="Binomial") <- fwer_pvals
 
@@ -91,12 +95,12 @@ singlePvalueBetaBinomial <- function(idx, k, n, mu, rho){
 
 singlePvalueBinomial <- function(idx, k, n, mu){
 
-  ki <- k[idx,]
-  ni <- n[idx,]
-  mui <- mu[idx,]
+    ki <- k[idx,]
+    ni <- n[idx,]
+    mui <- mu[idx,]
 
-  pvals <- pmin(1, pbinom(ki, ni, mui))
-  return (pvals)
+    pvals <- pmin(1, pbinom(ki, ni, mui))
+    return (pvals)
 }
 
 calculatePadjValues <- function(fds, type=currentType(fds), method="BY"){
@@ -124,9 +128,9 @@ calculatePadjValues <- function(fds, type=currentType(fds), method="BY"){
 
 getSiteIndex <- function(fds, type){
     ans <- switch(type,
-        psi5 = mcols(fds, type=type)[['startID']],
-        psi3 = mcols(fds, type=type)[['endID']],
-        psiSite = mcols(fds, type=type)[['spliceSiteID']]
+            psi5 = mcols(fds, type=type)[['startID']],
+            psi3 = mcols(fds, type=type)[['endID']],
+            psiSite = mcols(fds, type=type)[['spliceSiteID']]
     )
     return(ans)
 }
@@ -137,38 +141,39 @@ getGeneIDs <- function(fds, type){
     return(geneIDs)
 }
 
-getPvalsPerGene <- function(fds, type, pvals=pVals(fds, type=type), sampleID=NULL,
-                            method="holm", BPPARAM=parallel(fds)){
+getPvalsPerGene <- function(fds, type, pvals=pVals(fds, type=type),
+                    sampleID=NULL, method="holm", BPPARAM=parallel(fds)){
 
-  # extract data
-  dt <- data.table(
-    geneID=mcols(fds, type=type)$hgnc_symbol,
-    as.data.table(pvals) )
-  setkey(dt, geneID)
+    # extract data
+    dt <- data.table(
+            geneID=mcols(fds, type=type)$hgnc_symbol,
+            as.data.table(pvals))
+    setkey(dt, geneID)
 
-  samples <- samples(fds)
-  if(!is.null(sampleID)){
-    samples <- sampleID
-  }
+    samples <- samples(fds)
+    if(!is.null(sampleID)){
+        samples <- sampleID
+    }
 
-  pvalsPerGene <- matrix(unlist(bplapply(samples, BPPARAM=BPPARAM, function(i){
-    dttmp <- dt[,min(p.adjust(get(i), method=method)),by=geneID]
-    dttmp <- dttmp[!is.na(geneID),]
-    setkey(dttmp, geneID)
-    dttmp[J(getGeneIDs(fds, type)), V1]
-  })), ncol=length(samples))
+    pvalsPerGene <- matrix(unlist(bplapply(samples, BPPARAM=BPPARAM,
+        function(i){
+                dttmp <- dt[,min(p.adjust(get(i), method=method)),by=geneID]
+                dttmp <- dttmp[!is.na(geneID),]
+                setkey(dttmp, geneID)
+                dttmp[J(getGeneIDs(fds, type)), V1]
+        })), ncol=length(samples))
 
-  colnames(pvalsPerGene) <- samples
-  rownames(pvalsPerGene) <- na.omit(unique(dt$geneID))
-  
-  return(pvalsPerGene)
+    colnames(pvalsPerGene) <- samples
+    rownames(pvalsPerGene) <- na.omit(unique(dt$geneID))
+
+    return(pvalsPerGene)
 
 }
 
 getPadjPerGene <- function(pvals, method="BY"){
 
-  padjPerGene <- apply(pvals, 2, p.adjust, method=method)
+    padjPerGene <- apply(pvals, 2, p.adjust, method=method)
 
-  return(padjPerGene)
+    return(padjPerGene)
 
 }
