e2a5cfeed47ad06cf2433280df7a4cc14a3cb59a
Christian Mertes <mertes@in.tum.de>
2019-12-10 03:30:16 +0100

    better testing

--- setupEnv.R
+++ setupEnv.R
@@ -3,7 +3,8 @@ NCPUS <- 6
 
 print_log <- function(...){
     hash_line <- paste0(rep("#", 10), collapse="")
-    message(paste0("\n", hash_line, "\n### ", date(), ": ", ..., "\n", hash_line, "\n"))
+    message(paste0("\n", hash_line, "\n### ", date(), 
+            ": ", ..., "\n", hash_line, "\n"))
 }
 
 installIfReq <- function(p, type=BTYPE, Ncpus=NCPUS, ...){
@@ -25,18 +26,22 @@ INSTALL <- BiocManager::install
 if("windows" == .Platform$OS.type){
     print_log("Install XML on windows ...")
     BTYPE <- "win.binary"
-    installIfReq(p=c("XML", "xml2", "RSQLite", "progress", "AnnotationDbi", "BiocCheck"))
+    installIfReq(p=c("XML", "xml2", "RSQLite", "progress", 
+            "AnnotationDbi", "BiocCheck"))
     
     print_log("Install source packages only for windows ...")
-    INSTALL(c("GenomeInfoDbData", "org.Hs.eg.db", "TxDb.Hsapiens.UCSC.hg19.knownGene"), type="both")
+    INSTALL(c("GenomeInfoDbData", "org.Hs.eg.db", 
+            "TxDb.Hsapiens.UCSC.hg19.knownGene"), type="both")
 } else {
     BTYPE <- "source"
 }
 
 # install needed packages
-# add testthat to pre installation dependencies due to: https://github.com/r-lib/pkgload/issues/89
-for(p in c("XML", "xml2", "testthat", "devtools", "covr", "roxygen2", "BiocCheck", "R.utils", 
-            "GenomeInfoDbData", "rtracklayer", "hms")){
+# add testthat to pre installation dependencies 
+# due to: https://github.com/r-lib/pkgload/issues/89
+for(p in c("getopt", "XML", "xml2", "testthat", "devtools", "covr",
+            "roxygen2", "BiocCheck", "R.utils", "GenomeInfoDbData",
+            "rtracklayer", "hms")){
     installIfReq(p=p, type=BTYPE, Ncpus=NCPUS)
 }
 
@@ -47,6 +52,7 @@ R.utils::withTimeout(timeout=2400, {
         INSTALL(ask=FALSE, type=BTYPE, Ncpus=NCPUS)
     
         print_log("Install dev package")
-        devtools::install(".", dependencies=TRUE, upgrade=TRUE, type=BTYPE, Ncpus=NCPUS)
+        devtools::install(".", dependencies=TRUE, upgrade=TRUE, 
+                type=BTYPE, Ncpus=NCPUS)
     })
 })
--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -2,15 +2,17 @@
 # helper scripts for the testing step
 #
 getFraseR <- function(clean=FALSE){
-    if(!"initBPPARAM" %in% ls()){
-        if(.Platform$OS.type != "windows"){
-            register(MulticoreParam(min(4, bpworkers())))
-        } else {
-            register(SerialParam())
-        }
-    }
-    initBPPARAM <- TRUE
-    assign("initBPPARAM", initBPPARAM, envir=parent.env(environment()))
+    #if(!"initBPPARAM" %in% ls()){
+    #    
+    #    if(.Platform$OS.type != "windows"){
+    #        register(MulticoreParam(min(4, bpworkers())))
+    #        bpstart(bpparam())
+    #    } else {
+    #        register(SerialParam())
+    #    }
+    #}
+    #initBPPARAM <- TRUE
+    #assign("initBPPARAM", initBPPARAM, envir=parent.env(environment()))
     
     fds <- createTestFraseRDataSet(rerun=clean)
     fds
