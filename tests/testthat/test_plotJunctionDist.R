9fec52f324643800e58b3eac1fabb4c3e34bb298
Ines Scheller <scheller@in.tum.de>
2020-04-17 10:34:46 +0200

    lower cutoffs in results in the examples

--- tests/testthat/test_plotJunctionDist.R
+++ tests/testthat/test_plotJunctionDist.R
@@ -3,7 +3,7 @@ context("Test distribution plots for given results/junction")
 test_that("Main junction distribution plot", {
     # get results
     fds <- getFraseR()
-    res <- results(fds, padjCutoff=1, zScoreCutoff=NA, deltaPsiCutoff=0.1)
+    res <- results(fds, padjCutoff=1, zScoreCutoff=NA, deltaPsiCutoff=NA)
 
     # plot distributions
     expect_silent(plotExpression(fds, result=res[1]))
