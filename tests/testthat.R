ba756395d688b84f169abfaa9601f049fc771f77
Christian Mertes <mertes@in.tum.de>
2020-08-25 13:45:03 -0700

    run in single thread to speed up on small datasets.
    Especially the SnowParam option on windows is pretty slow

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,9 @@
 library(testthat)
 library(FRASER)
 
+# to speed up the testing on windows do it in serial mode
+if(.Platform$OS.type != "unix") {
+    register(SerialParam())
+}
+
 test_check("FRASER")
