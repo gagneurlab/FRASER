82011852ce24979f8d2725c51537cbefcd6d33eb
Christian Mertes <mertes@in.tum.de>
2020-08-25 17:04:22 -0700

    cleanup zScore computation

--- tests/testthat/test_stats.R
+++ tests/testthat/test_stats.R
@@ -1,34 +1,32 @@
 context("Test stats calculations")
 
 test_that("PSI value calculation", {
-    # get subset to speed up test
-    #fds <- getFraser()[,1]
-    #fds <- fds[mcols(fds, type="psi3")$startID %in% c(1,2,6,7),]
     
-    # expect it from the toy data after subsetting
-    #expect_equal(length(fds), 10)
-    #expect_equal(length(nonSplicedReads(fds)), 12)
-
-    # set predifiend values
-    #assays(fds)[["rawCountsJ"]]  <- rep(c(0,5), c(1,9))
-    #assays(fds)[["rawCountsSS"]] <- rep(c(0,5), c(1,11))
-
-    # calculate psi values
-    #fds <- calculatePSIValues(fds)
-
-    #assays(fds)[["psiSite"]]
-    #counts(fds, type="psi3")
-
-    #expect_equal("ok", "ok")
-    #settings <- createTestFraserDataSet()
-    #expect_is(settings, "FraserDataSet")
+    fds <- getFraser()
+    
+    psiVal <- K(fds, "psi5") / N(fds, "psi5")
+    
+    # no pseudo counts in psi values
+    expect_equal(psiVal[!is.na(psiVal)], assay(fds, "psi5")[!is.na(psiVal)])
+    
+    # NAs turned into 1
+    expect_equal(assay(fds, "psi5")[is.na(psiVal)], rep(1, sum(is.na(psiVal))))
+    
+    # NAs where N==0
+    expect_true(all(N(fds, "psi5")[!is.na(psiVal)] != 0))
+    expect_true(all(N(fds, "psi5")[ is.na(psiVal)] == 0))
 })
 
 test_that("Zscore calculation", {
-    #fds <- getFraser()
-    #fds <- FRASER(fds)
-
-    #expect_is(fds, "FraserDataSet")
-    #expect_equal(dim(fds), c(94, 12))
-    #expect_equal(dim(nonSplicedReads(fds)), c(111, 12))
+    fds <- getFraser(clean = TRUE)
+    
+    # prepare zScore input for logit scale
+    psiVal <- (K(fds, "psi5") + pseudocount())/(N(fds, "psi5") + 2*pseudocount())
+    mu <- predictedMeans(fds, "psi5")
+    residual <- qlogis(psiVal) - qlogis(mu)
+    
+    # compute zscore
+    zscores <- (residual - rowMeans(residual)) / rowSds(residual)
+    
+    expect_equal(zscores, zScores(fds, "psi5"))
 })
