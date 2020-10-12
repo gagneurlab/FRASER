79027d57eddb700345d5a88c727a0ae2c0547bd2
Christian Mertes <mertes@in.tum.de>
2020-10-12 12:02:31 -0700

    fix CI (travis/appveyor)

--- setupEnv.R
+++ setupEnv.R
@@ -1,10 +1,10 @@
-BTYPE <- "both"
+BTYPE <- ifelse(.Platform$OS.type == 'unix', "source", "both")
 NCPUS <- 6
+START_TIME <- Sys.time()
 
 print_log <- function(...){
     hash_line <- paste0(rep("#", 10), collapse="")
-    message(paste0("\n", hash_line, "\n### ", date(), 
-            ": ", ..., "\n", hash_line, "\n"))
+    message(paste0("\n", hash_line, "\n### ", date(), ": ", ..., "\n", hash_line, "\n"))
 }
 
 installIfReq <- function(p, type=BTYPE, Ncpus=NCPUS, ...){
@@ -16,33 +16,33 @@ installIfReq <- function(p, type=BTYPE, Ncpus=NCPUS, ...){
     }
 }
 
+# install Bioconductor
 if(!requireNamespace("BiocManager", quietly=TRUE)){
     print_log("Install BiocManager")
     install.packages("BiocManager", Ncpus=NCPUS)
 }
 INSTALL <- BiocManager::install
 
+# since the current XML package is not compatible with 3.6 anymore
+if(!requireNamespace("XML", quietly=TRUE) & R.version[['major']] == "3"){
+    installIfReq(p="devtools", type=BTYPE, Ncpus=NCPUS)
+    devtools::install_version("XML", version="3.99-0.3")
+}
+
 # because of https://github.com/r-windows/rtools-installer/issues/3
 if("windows" == .Platform$OS.type){
     print_log("Install XML on windows ...")
     BTYPE <- "win.binary"
-    installIfReq(p=c("XML", "xml2", "RSQLite", "progress", 
-            "AnnotationDbi", "BiocCheck"))
+    installIfReq(p=c("XML", "xml2", "RSQLite", "progress", "AnnotationDbi", "BiocCheck"))
     
     print_log("Install source packages only for windows ...")
-    INSTALL(c("GenomeInfoDbData", "org.Hs.eg.db", 
-            "TxDb.Hsapiens.UCSC.hg19.knownGene"), type="both")
+    INSTALL(c("GenomeInfoDbData", "org.Hs.eg.db", "TxDb.Hsapiens.UCSC.hg19.knownGene"), type="both")
 } else {
     BTYPE <- "source"
 }
 
 # install needed packages
-# add testthat to pre installation dependencies 
-# due to: https://github.com/r-lib/pkgload/issues/89
-if(!requireNamespace("XML", quietly=TRUE) & R.version[['major']] == "3"){
-    installIfReq(p="devtools", type=BTYPE, Ncpus=NCPUS)
-    devtools::install_version("XML", version="3.99-0.3")
-}
+# add testthat to pre installation dependencies due to: https://github.com/r-lib/pkgload/issues/89
 for(p in c("getopt", "XML", "xml2", "testthat", "devtools", "covr",
             "roxygen2", "BiocCheck", "R.utils", "GenomeInfoDbData",
             "rtracklayer", "hms")){
@@ -68,8 +68,9 @@ R.utils::withTimeout(timeout=2400, {
 # fix knitr for 3.6 for more details see BiocStyle issue 78
 # https://github.com/Bioconductor/BiocStyle/issues/78
 if(R.version[['major']] == "3"){
+    options(repos=c(CRAN="http://cran.rstudio.com"))
     installIfReq(p="devtools", type=BTYPE, Ncpus=NCPUS)
-    devtools::install_version("knitr", version="1.29")
+    devtools::install_version("knitr", version="1.29", type="source")
 }
 
 print(BiocManager::valid())
