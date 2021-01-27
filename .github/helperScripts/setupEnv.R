47d25a97bc4f52921e6cf0324b908492cbec20f7
Christian Mertes <mertes@in.tum.de>
2021-01-27 15:22:48 +0100

    only install biomaRt testing in r-release

--- .github/helperScripts/setupEnv.R
+++ .github/helperScripts/setupEnv.R
@@ -75,7 +75,10 @@ if(R.version[['major']] == "3"){
     BiocManager::install(ask=FALSE, update=FALSE, c(
             "Bioconductor/BiocFileCache", "grimbough/biomaRt", "yihui/knitr@v1.29"))
 } else {
-    BiocManager::install("grimbough/biomaRt", ref="3_12_testing")
+    inst_biomaRt_version <- as.character(utils::packageVersion("biomaRt"))
+    if(utils::compareVersion("2.46.2", inst_biomaRt_version) == 1){
+        BiocManager::install("grimbough/biomaRt", ref="3_12_testing")
+    }
 }
 
 # to get FRASER session info
