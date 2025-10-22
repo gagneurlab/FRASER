ff84447d505fcf2c38be5cbe3353c52838f8f333
AtaJadidAhari <atajadidahari@gmail.com>
2025-10-22 13:29:40 +0200

    test annotation_methods

--- tests/testthat/test_annotation_methods.R
+++ tests/testthat/test_annotation_methods.R
@@ -2,4 +2,29 @@ context("Annotations methods")
 
 test_that("annotateRanges", {
     expect_equal("TIMMDC1", "TIMMDC1")
+
+    fds <- createTestFraserSettings()
+    fds <- countRNAData(fds)
+
+    # compute stats
+    fds <- calculatePSIValues(fds)
+    # filter junctions with low expression
+    fds <- filterExpressionAndVariability(fds, minExpressionInOneSample=20,
+					  minDeltaPsi=0.0, filter=TRUE)
+
+    txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+    orgDb <- org.Hs.eg.db
+
+    # remove a splice site
+    subset_fds <- fds[-c(1), , by = "ss"]
+
+    # check to see if it is correctly removed
+    expect_false(1 %in% mcols(subset_fds, type="theta")$spliceSiteID)
+
+    # annoate genes
+    subset_fds <- annotateRangesWithTxDb(subset_fds, txdb=txdb, orgDb=orgDb)
+
+    # check if the junction is annotated
+    j_mcols <- mcols(subset_fds, type = "j")
+    expect_false(is.na(j_mcols$hgnc_symbol[j_mcols$startID == 1]))
 })
